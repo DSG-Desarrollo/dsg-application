9c8ea8e96acad06d6917cbe63dc75028ea01e854
Julio Flores <cesarff.julio@g,mail.com>
2024-06-18 10:04:43 -0600

    feat: :fire: Generando la APK
    Se ha creado una rama nueva para desplejar el apk, aunque esta versión que se esta subiendo nos hizo que bajaramos de versión

--- app.config.js
+++ app.config.js
@@ -6,5 +6,8 @@ export default ({ config }) => ({
   extra: {
     wsERPURL: process.env.WS_BASE_URL,
     DBNAME: process.env.DB_NAME,
+    eas: {
+      projectId: 'f990977d-e454-427d-865b-27481130d47a',
+    },
   },
 });
