55b9ee121daaad2a1a815430991571efce41cce0
DSG <desarrollo@proteccionycontrol.com>
2025-02-23 16:41:02 -0600

    feat: Configuración para la subida a la playstore
    Config8uración inicial y estable para la play store

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default ({ config }) => ({
     wsERPURL: process.env.WS_BASE_URL,
     DBNAME: process.env.DB_NAME,
     eas: {
-      projectId: 'b19a1a48-d69b-4f7d-8b5f-b5aafb61b42d'
+      projectId: '3b8289e8-09fa-4ae3-9276-41427d6d1f9d'
     }
   },
 });
