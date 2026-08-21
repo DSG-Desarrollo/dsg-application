cc0ca5cd8cad00ad2ddd587fad368037841e03b7
Julio Flores <cesarff.julio@g,mail.com>
2026-08-21 15:56:38 -0600

    feat: :art: dividiendo los estilos

--- src/screens/App/WorkOrders/TabInstallationSignatureProof.jsx
+++ src/screens/App/WorkOrders/TabInstallationSignatureProof.jsx
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect } from "react";
 import {
   View,
   TextInput,
-  StyleSheet,
   ToastAndroid,
   KeyboardAvoidingView,
   ScrollView,
@@ -17,6 +16,7 @@ import ActionButtons from "@components/atoms/ActionButtons";
 import ApiService from "@services/api/ApiService";
 import FormValidation from "@components/molecules/FormValidation";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { signature as styles } from "./styles";
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
@@ -188,82 +188,5 @@ const TabInstallationSignatureProof = ({ route }) => {
     </KeyboardAvoidingView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    justifyContent: "center",
-  },
-  formContainer: {
-    flex: 1,
-    justifyContent: "space-between",
-  },
-  imageContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  canvasContainer: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  fixedImage: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
-  },
-  input: {
-    height: 50,
-    fontSize: 16,
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-    color: "#333",
-    backgroundColor: "#f7f7f7",
-    borderRadius: 10,
-  },
-  underline: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#888",
-  },
-  inputError: {
-    borderColor: "red",
-    borderWidth: 1,
-  },
-  errorText: {
-    color: "#C0392B",
-    fontSize: 14,
-    fontWeight: "bold",
-  },
-  buttonContainer: {
-    marginTop: 20,
-  },
-  customButtonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  customButton: {
-    backgroundColor: "#ff6347",
-    borderRadius: 10,
-    padding: 10,
-    marginHorizontal: 5,
-  },
-  errorContainer: {
-    marginTop: 5,
-    backgroundColor: "#F8D7DA",
-    padding: 5,
-    borderRadius: 5,
-  },
-  customButtonText: {
-    color: "#fff",
-    fontSize: 18,
-    textAlign: "center",
-  },
-});
 
 export default TabInstallationSignatureProof;
--- src/screens/App/WorkOrders/styles/index.js
+++ src/screens/App/WorkOrders/styles/index.js
@@ -1,5 +1,6 @@
 export { common } from './common';
 export { installation } from './installation';
 export { location } from './location';
+export { signature } from './signature';
 export { supplies } from './supplies';
 export { unitDetail } from './unit_detail';
