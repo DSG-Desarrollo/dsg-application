c38af7aa8f7eac9e6845306ef88b817af532d609
Julio Flores <cesarff.julio@g,mail.com>
2026-09-11 13:26:44 -0600

    feat: :art: panel de sync

--- src/utils/NetworkInfo.jsx
+++ src/utils/NetworkInfo.jsx
@@ -41,6 +41,7 @@ const NetworkInfo = ({ children }) => {
                     {statusText}
                 </View>
             </View>
+            {__DEV__ && <DevSyncPanel />}
             {children}
         </View>
     );
